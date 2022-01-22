be6848a037e63ff3e2f9f29106ac0ffbffbbe37b
raghu1282 <31691445+raghu1282@users.noreply.github.com>
2022-01-22 18:56:04 +0530

    added base methods
    Adding base methods

---
+++ ConsoleAppForCompany/CalculatePromotions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppForCompany
+{
+    public class CalculatePromotions
+    {
+        Double totalAmt = 0;
+
+        public void GetTotalAmountWithPromotions(Double AUnits,Double BUnits,Double CUnits, Double DUnits)
+        {
+
+        }
+
+    }
+
+    public static class Utility
+    {
+        public static void Calculate()
+        {
+
+        }
+
+    }
+}
--- ConsoleAppForCompany/Program.cs
+++ ConsoleAppForCompany/Program.cs
@@ -4,9 +4,20 @@ namespace ConsoleAppForCompany
 {
     class Program
     {
+        enum SKUIDs_Units: int
+        {
+            SKUIDs_A_UnitPrice = 50,
+            SKUIDs_B_UnitPrice = 30,
+            SKUIDs_C_UnitPrice = 20,
+            SKUIDs_D_UnitPrice = 15,
+            SKUIDs_A_Promotion_Price = 130,
+            SKUIDs_B_Promotion_Price = 45,
+            SKUIDs_CnD_Promotion_Price = 30,
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
